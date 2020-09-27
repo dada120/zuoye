4a7a11171e9bfcbbed8eacdd8955b6102e44f9a7
dada120 <70931616+dada120@users.noreply.github.com>
2020-09-28 00:49:40 +0800

    创新， 写入

---
+++ demo.py
@@ -0,0 +1,12 @@
+filename='demo.py'
+with open(filename, 'r') as fp:
+    lines = fp.readlines()
+maxLength = len(max(lines, key=len))
+
+lines = [line.rstrip().ljust(maxLength)+'#'+str(index)+'\n'
+         # string.rstrip()，可以删除 string 字符串末尾的指定字符（默认为空格）
+         # string.ljust()， 返回一个原字符串左对齐,并使用空格填充至指定长度的新字符串。如果指定的长度小于原字符串的长度则返回原字符串。
+         for index, line in enumerate(lines)]
+with open(filename[:-3]+'_new.py', 'w') as fp:
+    fp.writelines(lines)
+
