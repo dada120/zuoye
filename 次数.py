2fb77adfbe0bbbf815eaa90da7a39f081d39c2d7
dada120 <70931616+dada120@users.noreply.github.com>
2020-09-29 11:12:42 +0800

    Update 次数.py

--- 次数.py
+++ 次数.py
@@ -1,7 +1,8 @@
 lst= eval(input('请输入列表list：'))
 def qiuou(x):
-    sum=0  # can't assign to function call
-    # 圆括号()表示函数调用 方括号[]表示列表值的引用 你当然应该用方括号了,这是一个容易犯的错误
+    '''求列表里存在的偶数次数'''
+    sum=0  #错误： can't assign to function call
+           # 圆括号()表示函数调用 方括号[]表示列表值的引用 你当然应该用方括号了,这是一个容易犯的错误
     for i in x:
         if i%2==0:
             sum+=1
